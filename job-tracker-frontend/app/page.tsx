00ae9a7ec5c019e7d212881bbf389b198291a106
fahritech04 <fahricoding@gmail.com>
2026-07-09 13:21:50 +0700

    feat(job-tracker-frontend): installation

---
+++ job-tracker-frontend/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-50">
+      <h1 className="text-4xl font-bold text-blue-600">Job Tracker App</h1>
+      <p className="mt-4 text-gray-600">Frontend Next.js berhasil berjalan bersih!</p>
+    </main>
+  );
+}
