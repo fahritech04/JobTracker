338c509df4009cd4318be3fbc2e5cf61ab64f412
fahritech04 <fahricoding@gmail.com>
2026-07-09 13:07:27 +0700

    feat(model: application, user, api): update

--- job-tracker-backend/routes/api.php
+++ job-tracker-backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ApplicationController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -14,6 +15,7 @@ Route::middleware('auth:sanctum')->group(function () {
     });
     
     Route::post('/logout', [AuthController::class, 'logout']);
-    
+
+    Route::apiResource('applications', ApplicationController::class);
     // Nanti kita akan meletakkan rute CRUD Applications di sini
 });
